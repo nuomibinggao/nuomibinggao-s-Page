ef82ce1ba361164d6405024e288e7cfd160a6aca
nuomibinggao <Sicheng_Guide@hotmail.com>
2025-11-08 21:43:05 +0800

    Started migration to the Svelte framework

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<section class="intro">
+  <h2><img src="/icons/waving_hand.svg" alt="Waving hand" class="icon icon-large">Welcome</h2>
+  <p>I'm <b>nuomibinggao</b>, a student passionate about technology and games. This minimal space showcases my projects and provides ways to connect with me.</p>
+  <p>Feel free to explore my work and reach out through the links below.</p>
+  <p>Why "Melty Pages"? Because the bugs on this website makes it melt down... Slowly...</p>
+  <p>This website is open source on <a href="https://github.com/nuomibinggao/Melty-Pages" target="_blank" rel="noopener"><img src="/icons/code.svg" alt="Code" class="icon icon-inline">GitHub</a>.</p>
+  <a href="/changelog" class="link-button"><img src="/icons/update.svg" alt="Update" class="icon icon-inline">View Website Changelog</a>
+  <a href="https://github.com/nuomibinggao/Melty-Pages/issues" target="_blank" rel="noopener" class="link-button"><img src="/icons/bug_report.svg" alt="Report" class="icon icon-inline">Report an Issue</a>
+</section>
+
+<div class="sections">
+
+  <section class="section-card" id="studios">
+    <h3><img src="/icons/diversity_3.svg" alt="Studios" class="icon icon-large">Melty Studios</h3>
+    <p>This is our team portfolio website, it is still under construction. Maybe come back later...</p>
+  </section>
+
+  <section class="section-card" id="projects">
+    <h3><img src="/icons/folder.svg" alt="Folder" class="icon icon-large">Projects Hub</h3>
+    <p>Browse through my various projects and contributions.</p>
+    <a href="/projecthub" class="link-button"><img src="/icons/arrow_forward_ios.svg" alt="Arrow forward" class="icon icon-inline">View Projects</a>
+  </section>
+
+  <section class="section-card" id="adofai">
+    <h3><img src="/icons/adofai_logo.png" alt="Groups" class="icon icon-adofai"></h3>
+    <p>This is a section dedicated to ADOFAI content, see our custom levels and other resources here.</p>
+    <a href="/adofai-index" class="link-button"><img src="/icons/arrow_forward_ios.svg" alt="Arrow forward" class="icon icon-inline">View Section</a>
+  </section>
+
+  <section class="section-card" id="links">
+    <h3><img src="/icons/add_link.svg" alt="Link" class="icon icon-large">Connect</h3>
+    <p>Find me across various platforms and social media.</p>
+    <a href="/links" class="link-button"><img src="/icons/arrow_forward_ios.svg" alt="Arrow forward" class="icon icon-inline">My Links</a>
+  </section>
+</div>
