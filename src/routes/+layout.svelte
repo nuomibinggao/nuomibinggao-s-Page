ee77168f9310a5a3dce2d8c4fda58b0635a07cd2
nuomibinggao <Sicheng_Guide@hotmail.com>
2025-11-08 22:30:48 +0800

    Migrated index.html to +page.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,8 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link href="https://api.fontshare.com/css?f[]=chillax@400,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style-v2.css">
 </svelte:head>
 
 {@render children()}
